871f489f4506d147db29bac46ecce2550de80eb8
Olga Gata <olgagatay@gmail.com>
2023-08-25 14:15:47 +0300

    added fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".header").addClass("header-fixed");
+    } else {
+      $(".header").removeClass("header-fixed");
+    }
+  });
+
   $(".categories__filter-btn").on("click", function () {
     $(".categories__filter-btn").removeClass("categories__filter-btn--active");
     $(this).addClass("categories__filter-btn--active");
